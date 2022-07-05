31be5ca358bbd251e549fa2d2e966d567e0376e1
Fountain <fountainaigbona@gmail.com>
2022-07-05 05:03:07 +0100

    done with task 9 corrections

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,9 +1,13 @@
 #!/usr/bin/python3
+
+
 def max_integer(my_list=[]):
+    """Finds the biggest number in a list"""
     if len(my_list) == 0:
         return (None)
-    biggest_integer = 0
-    for integer in my_list:
-        if integer > biggest_integer:
-            biggest_integer = integer
+
+    biggest_integer = my_list[0]
+    for i in range(len(my_list)):
+        if my_list[i] > biggest_integer:
+            biggest_integer = my_list[i]
     return (biggest_integer)
