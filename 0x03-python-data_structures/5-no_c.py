d4c34bbb1c4215a7813fe8697da6f1fd467a56d5
Fountain <fountainaigbona@gmail.com>
2022-07-05 04:02:08 +0100

    done with task 5

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,10 +1,10 @@
 #!/usr/python3
+
+
 def no_c(my_string):
-    i = 0
-    for a in my_string:
-        if a == 'c' or a == 'C':
-            i+=1
-            continue
-        else:
-            print(a, end='')
-            i+=1
+    """ returns a new string with all the c and C's removed """
+    new_string = ""
+    for i in range(len(my_string)):
+        if my_string[i] != 'c' and my_string[i] != 'C':
+            new_string += my_string[i]
+    return (new_string)
