7f51acb0c592c1a93d30ef67d6bc199f8e9e5218
Fountain <fountainaigbona@gmail.com>
2022-07-05 04:46:37 +0100

    done with task 6

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
+
 def print_matrix_integer(matrix=[[]]):
-    if not matrix:
-        return ('')
-    for block in matrix:
-        for idx in range(len(block)):
-            if idx < (len(block) - 1):
-                print('{}'.format(block[idx]), end=' ')
-            else:
-                print('{}'.format(block[idx]), end='')
-        print(end='\n')
+    """Function that prints a matrix of integers"""
+    for i in range(len(matrix)):
+        for j in range(len(matrix[i])):
+            if j != 0:
+                print(" ", end='')
+            print("{:d}".format(matrix[i][j]), end='')
+        print()
