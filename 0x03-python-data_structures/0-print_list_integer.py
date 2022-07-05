00f5904b7750bbd214c9c9e0f9aa8f7ede1bd447
Fountain <fountainaigbona@gmail.com>
2022-07-05 03:14:16 +0100

    hot fix for task 0

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
+
+
 def print_list_integer(my_list=[]):
-    """Prints all integers of a list"""
+    """
+    Prints all integers of a list
+    """
     for a in range(len(my_list)):
         print('{:d}'.format(my_list[a]))
 
