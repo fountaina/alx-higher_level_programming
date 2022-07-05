5218bdac7a75c27d5731caeda2cefa13252b4a89
Fountain <fountainaigbona@gmail.com>
2022-07-05 03:22:22 +0100

    done with fixing task 3 errors

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -3,6 +3,8 @@
 
 def print_reversed_list_integer(my_list=[]):
     """ Prints the list of integers in reverse """
+    if len(my_list) == 0:
+        return (None)
     a = 1
     while a <= len(my_list):
         print('{:d}'.format(my_list[-a]))
