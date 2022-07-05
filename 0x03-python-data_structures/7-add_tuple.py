69bff1633901c87f3f88514e2da2a4ced33669b3
Fountain <fountainaigbona@gmail.com>
2022-07-05 04:57:28 +0100

    fixed issues with task 7

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
+
+
 def add_tuple(tuple_a=(), tuple_b=()):
+    """ adds tuples together """
     if len(tuple_a) >= 2:
-        first_tup_0  = tuple_a[0]
-        first_tup_1  = tuple_a[1]
+        first_tup_0 = tuple_a[0]
+        first_tup_1 = tuple_a[1]
     if len(tuple_b) >= 2:
         second_tup_0 = tuple_b[0]
         second_tup_1 = tuple_b[1]
@@ -11,12 +14,12 @@ def add_tuple(tuple_a=(), tuple_b=()):
             first_tup_1 = 0
             first_tup_0 = tuple_a[0]
         else:
-            first_tup_1, first_tup_0 = (0,0)
-    if len (tuple_b) < 2:
+            first_tup_1, first_tup_0 = (0, 0)
+    if len(tuple_b) < 2:
         if len(tuple_b) == 1:
             second_tup_1 = 0
             second_tup_0 = tuple_b[0]
         else:
-            second_tup_0, second_tup_1 = (0,0)
+            second_tup_0, second_tup_1 = (0, 0)
     new_tuple = (first_tup_0 + second_tup_0, first_tup_1 + second_tup_1)
     return (new_tuple)
